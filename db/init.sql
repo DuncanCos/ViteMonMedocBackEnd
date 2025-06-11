9ca9f4e096ced1f06dd78e7000ffe07ebadfcd8c
dundun <dcostes@aveco.fr>
2025-06-11 11:30:52 +0200

    modif db

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,11 @@
+CREATE TABLE IF NOT EXISTS "Users" (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS "Patients" (
     id SERIAL PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
@@ -7,18 +15,18 @@ CREATE TABLE IF NOT EXISTS "Patients" (
     taille DECIMAL(5, 2),
     traitement_en_cours TEXT,
     medicament TEXT,
-    medecin VARCHAR(255),
+    medecin_id INT NOT NULL,
     notes TEXT,
     rdv TIMESTAMP,
     statut VARCHAR(50),
     numero_de_telephone VARCHAR(15),
-    last_update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    mail VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_medecin
+        FOREIGN KEY (medecin_id)
+        REFERENCES "Users"(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS "Users" (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);

44f3a93af4ab25790c4bd7e7f1ead864ead3be50
dundun <dcostes@aveco.fr>
2025-06-12 16:03:30 +0200

    ajout route patientmedecin + ajout mail pour patient dans crud

--- express-postgres-crud-app/src/controllers/patientController.js
+++ express-postgres-crud-app/src/controllers/patientController.js
@@ -1,4 +1,5 @@
 const { pool } = require('../config/database'); // Ajustez le chemin selon votre structure
+const jwt = require('jsonwebtoken');
 
 exports.getAllPatient = async (req, res) => {
     try {
@@ -30,13 +31,13 @@ exports.getPatientById = async (req, res) => {
 exports.createPatient = async (req, res) => {
     try {
         console.log('Received request body:', req.body);
-        const { nom, prenom, age, poids, taille, traitement_en_cours, medicament, medecin, notes, rdv, statut, numero_de_telephone } = req.body;
+        const { nom, prenom, age, poids, taille, traitement_en_cours, medicament, medecin, notes, rdv, statut, numero_de_telephone,mail } = req.body;
 
         const result = await pool.query(
             `INSERT INTO "Patients" 
-            (nom, prenom, age, poids, taille, traitement_en_cours, medicament, medecin, notes, rdv, statut, numero_de_telephone) 
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *`,
-            [nom, prenom, age, poids, taille, traitement_en_cours, medicament, medecin, notes, rdv, statut, numero_de_telephone]
+            (nom, prenom, age, poids, taille, traitement_en_cours, medicament, medecin_id, notes, rdv, statut, numero_de_telephone, mail) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *`,
+            [nom, prenom, age, poids, taille, traitement_en_cours, medicament, medecin, notes, rdv, statut, numero_de_telephone,mail]
         );
 
         res.status(201).json(result.rows[0]);
@@ -49,13 +50,13 @@ exports.createPatient = async (req, res) => {
 exports.updatePatient = async (req, res) => {
     try {
         const { id } = req.params;
-        const { nom, prenom, age, poids, taille, traitement_en_cours, medicament, medecin, notes, rdv, statut, numero_de_telephone } = req.body;
+        const { nom, prenom, age, poids, taille, traitement_en_cours, medicament, medecin, notes, rdv, statut, numero_de_telephone , mail} = req.body;
 
         const result = await pool.query(
             `UPDATE "Patients" 
-            SET nom = $1, prenom = $2, age = $3, poids = $4, taille = $5, traitement_en_cours = $6, medicament = $7, medecin = $8, notes = $9, rdv = $10, statut = $11, numero_de_telephone = $12 
-            WHERE id = $13 RETURNING *`,
-            [nom, prenom, age, poids, taille, traitement_en_cours, medicament, medecin, notes, rdv, statut, numero_de_telephone, id]
+            SET nom = $1, prenom = $2, age = $3, poids = $4, taille = $5, traitement_en_cours = $6, medicament = $7, medecin = $8, notes = $9, rdv = $10, statut = $11, numero_de_telephone = $12 , mail = $13
+            WHERE id = $14 RETURNING *`,
+            [nom, prenom, age, poids, taille, traitement_en_cours, medicament, medecin, notes, rdv, statut, numero_de_telephone, id, mail]
         );
 
         if (result.rows.length === 0) {
@@ -81,6 +82,35 @@ exports.deletePatient = async (req, res) => {
 
         res.json({ message: 'Patient supprimé avec succès' });
     } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.getPatientFromMedecin = async (req, res) => {
+
+    // Récupérer l'ID du médecin depuis le jwt toker authorization header
+     const token = req.headers.authorization?.split(' ')[1];
+    
+        if (!token) {
+            return res.status(401).json({ message: 'Access denied. No token provided.' });
+        }
+    
+        try {
+            console.log('Received token:', token);
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decoded;
+           
+        } catch (error) {
+            res.status(403).json({ message: 'Invalid token.',error: error.message });
+        } 
+        console.log('Decoded user:', req.user.id);
+
+
+    try {
+        const result = await pool.query('SELECT * FROM "Patients" WHERE medecin_id = $1', [req.user.id]);
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error fetching patients:', error);
         res.status(500).json({ error: error.message });
     }
 };
--- express-postgres-crud-app/src/routes/patientRoutes.js
+++ express-postgres-crud-app/src/routes/patientRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/',  patientController.createPatient);
 router.get('/',  patientController.getAllPatient);
+router.get('/medecin/',  patientController.getPatientFromMedecin);
 router.get('/:id', patientController.getPatientById);
 router.put('/:id', patientController.updatePatient);
 router.delete('/:id', patientController.deletePatient);
